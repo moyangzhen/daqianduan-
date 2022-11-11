eac4216612967ea698c87fb271099d59b1634aa7
ludaohua <15711476734@163.com>
2022-11-11 12:27:43 +0800

    添加接口,格式化json数据,格式化body

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 const koa=require('koa');
 const Router=require('koa-router')
-const cors=require('@koa/cors');
-const koaBody=require('koa-body')
+const cors=require('koa2-cors');
+const parser = require('koa-parser')
+const json=require('koa-json')
 const app=new koa();
 const router=new Router();
+router.prefix('/api')
 router.get('/',ctx=>{
     console.log(ctx)
     console.log(ctx.request)
     ctx.body='hello world'
 })
 router.get('/api',ctx=>{
-    console.log(ctx)
-    console.log(ctx.request)
-    ctx.body='hello api'
-})
-router.get('/async',async(ctx)=>{
-    let result=await new Promise((resolve)=>{
-        setTimeout(() => {
-            resolve('Hello world 2s later')
-        }, 2000);
-    })
-    ctx.body=result
-})
-
-router.post('/post',async(ctx)=>{
-    let {body}=ctx.request
-    console.log(body);
-    console.log(ctx.request)
+    //get params
+    const params=ctx.request.query;
+    console.log(params);
+    //name:'imooc' age:18
+    console.log(params.name,params.age)
     ctx.body={
-        ...body
-    }
+        name:params.name,
+        age:params.age}
+    })
+router.post('/post',async(ctx)=>{
+    if (ctx.request.body !== undefined) {
+        const {body}=ctx.request
+        console.log(body)
+        ctx.body = {...body}
+      }
 })
-app.use(koaBody)
-app.use(cors)
-
+app.use(cors({
+    origin: function (ctx) {
+        // if (ctx.url === '/test') {
+            return "*"; // 允许来自所有域名请求
+        // }
+        // return "http://localhost:8080"; // 这样就能只允许 http://localhost:8080 这个域名的请求了
+    },
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 5,
+    credentials: true, // 当设置成允许请求携带cookie时，需要保证"Access-Control-Allow-Origin"是服务器有的域名，而不能是"*";
+    allowMethods: ['GET', 'POST', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
+// app.use(koaBody)
+app.use(parser())
+app.use(json({pretty:false,param:'pretty'}))
 //1.request ,method,response
 //2.api url=>function,router?
 //3.ctx,async
